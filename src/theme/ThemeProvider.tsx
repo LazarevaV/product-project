643807f74393c748bad13841b99873ccb84b9225
ViktiriiaLazareva <vilavika91@gmail.com>
2025-01-07 11:46:48 +0800

    first commit

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,24 @@
+import React, {FC,useState, useMemo} from 'react';
+import { LOCAL_STORAGE_THEM_KEY, Theme, ThemeContext } from './ThemeContext';
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEM_KEY) as Theme || Theme.LIGHT;
+
+const ThemeProvider: FC = ({children}) => {
+
+	const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+	const defaultProps = useMemo(() => ({
+			theme: theme,
+			setTheme: setTheme,
+	}), [theme])
+
+
+	return (
+
+		<ThemeContext.Provider value={defaultProps}>
+			{children}
+		</ThemeContext.Provider>
+
+	);
+};
+export default ThemeProvider;
